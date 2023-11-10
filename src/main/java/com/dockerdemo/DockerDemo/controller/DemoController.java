86b4347fc6413913ad80aff21d04809df963e029
anirudhramanan <anirudhramanan23@gmail.com>
2023-11-10 17:37:06 +0530

    rest contoller

--- src/main/java/com/dockerdemo/DockerDemo/controller/DemoController.java
+++ src/main/java/com/dockerdemo/DockerDemo/controller/DemoController.java
@@ -1,14 +1,14 @@
 package com.dockerdemo.DockerDemo.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping(path = "/dummy")
+@RestController
+@RequestMapping(path = "/api/v1")
 public class DemoController {
-    @GetMapping
+    @PostMapping(value = "/dummy")
     public ResponseEntity dummy() {
         return ResponseEntity.ok("Hello World");
     }
